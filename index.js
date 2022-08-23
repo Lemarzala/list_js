a6e1bf685a47cb5cde6481728c6129104fe9c0c3
lemarzala <lemarzala.tarakhil123@gmail.com>
2022-08-23 05:18:04 -0600

    push

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 
 const app = express();
-
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send(`
@@ -18,11 +15,28 @@ app.get("/", (req, res) => {
   `);
 });
 
-app.post("/", (req, res) => {
+const bodyParser = (req, res, next) => {
+  if (req.method === "POST") {
+    req.on("data", (data) => {
+      const parsed = data.toString("utf8").split("&");
+      const formData = {};
+      for (let pair of parsed) {
+        const [key, value] = pair.split("=");
+        formData[key] = value;
+      }
+      req.body = formData;
+      next();
+    });
+  } else {
+    next();
+  }
+};
+
+app.post("/", bodyParser, (req, res) => {
   console.log(req.body);
   res.send("Account created!!!");
 });
 
-app.listen(2000, () => {
+app.listen(3000, () => {
   console.log("Listening");
 });
