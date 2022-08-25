399eaf43e3338e580e7939360074e8702762ad95
lemarzala <lemarzala.tarakhil123@gmail.com>
2022-08-25 12:41:18 -0600

    push

--- repositories/users.js
+++ repositories/users.js
@@ -1,9 +1,19 @@
+const fs = require("fs");
+
 class UsersRepository {
   constructor(filename) {
     if (!filename) {
       throw new Error("creating a repository requires a filename");
     }
+    this.filename = filename;
+    try {
+      fs.accessSync(this.filename);
+    } catch (err) {
+      fs.writeFileSync(this.filename, "[]");
+    }
   }
+
+  async checkForFile() {}
 }
 
-new UsersRepository();
+const repo = new UsersRepository("users.json");
