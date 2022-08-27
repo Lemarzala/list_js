05b9aabd89be9017716ec7fc23927ed8d12a9eff
lemarzala <lemarzala.tarakhil123@gmail.com>
2022-08-26 21:07:27 -0600

    push

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const usersRepo = require("./repositories/users");
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send(`
@@ -15,25 +19,23 @@ app.get("/", (req, res) => {
   `);
 });
 
-const bodyParser = (req, res, next) => {
-  if (req.method === "POST") {
-    req.on("data", (data) => {
-      const parsed = data.toString("utf8").split("&");
-      const formData = {};
-      for (let pair of parsed) {
-        const [key, value] = pair.split("=");
-        formData[key] = value;
-      }
-      req.body = formData;
-      next();
-    });
-  } else {
-    next();
-  }
-};
+app.post("/", async (req, res) => {
+  const { email, password, passwordConfirmation } = req.body;
 
-app.post("/", bodyParser, (req, res) => {
-  console.log(req.body);
+  const existingUser = await usersRepo.getOneBy({ email });
+  if (existingUser) {
+    return res.send("Email in use");
+  }
+
+  if (password !== passwordConfirmation) {
+    return res.send("Passwords must match");
+  }
+
+  // Create a user in our user repo to represent this person
+  const user = await usersRepo.create({ email, password });
+
+  // Store the id of that user inside the users cookie
+
   res.send("Account created!!!");
 });
 
